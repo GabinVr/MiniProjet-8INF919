badff40a4e414198386f6d48002e3abc0a7660f2
Vrillault Gabin <gabin.vrillault@ecole.ensicaen.fr>
2025-12-16 17:40:21 +0100

    Starting 1.10.1

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -6,13 +6,20 @@ local_path = os.path.dirname(__file__)
 test_path = os.path.join(local_path, 'test.csv')
 train_path = os.path.join(local_path, 'train.csv')
 
+
 def load_data() -> pd.DataFrame:
     """
     Load the training and testing data from CSV files 
+    clean the data
     and return them as pandas DataFrames.
     """
     train_data = pd.read_csv(train_path)
     test_data = pd.read_csv(test_path)
+
+    # Data cleaning: remove rows with missing values
+    train_data.dropna(inplace=True)
+    test_data.dropna(inplace=True)
+
     return train_data, test_data
 
 if __name__ == "__main__":
